efdc223ff7ba78ae427764f970c1a3155eea5aeb
Andriy Savchenko <firkeuf@gmail.com>
2023-05-05 16:25:27 +0300

    Replace deprecated hass.config_entries.async_setup_platforms

--- custom_components/sora/__init__.py
+++ custom_components/sora/__init__.py
@@ -26,7 +26,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     await coordinator.async_config_entry_first_refresh()
     entry.async_on_unload(entry.add_update_listener(update_listener))
     hass.data.setdefault(DOMAIN, {})[entry.entry_id] = coordinator
-    hass.config_entries.async_setup_platforms(entry, PLATFORMS)
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
 
